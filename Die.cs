073236526b341ae8df6e8a8336b8a7a3d4d0f656
Darian Lafontaine <339571119@GoTVDSB.ca>
2023-10-24 10:26:27 -0400

    Add project files.

---
+++ Die.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Topic_5._5_More_Classes
+{
+    public class Die
+    {
+        private int _roll;
+        private Random _generator;
+
+        public Die()
+        {
+            _generator = new Random();
+            _roll = _generator.Next(1, 7);
+
+
+        }
+
+        public Die(int roll)
+        {
+            _generator = new Random();
+            _roll = roll;
+        }
+
+        public int Roll
+        {
+            get { return _roll; }
+        }
+
+        public override string ToString()
+        {
+            return _roll + "";
+        }
+
+        public int RollDie()
+        {
+            _roll = _generator.Next(1, 7);
+            return _roll;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Topic_5._5_More_Classes
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Die die1 = new Die();
+            Die die2 = new Die();
+
+            double bankMoney = 100.0;
+            double bet;
+            string decision;
+
+            Console.WriteLine("Welcome to the Casino!");
+            Console.WriteLine("How much would you likr to bet on the dice?");
+            bet = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine($"You have bet {bet}$.");
+            Console.WriteLine("would you like to bet on even sum, odd sum, double or not double?");
+            Console.WriteLine("for even and odd sum, you will win or lose your bet.");
+            Console.WriteLine("for double you will win/lose double your bet, and for not double you will win/lose half your bet");
+            decision = Console.ReadLine().ToLower();
+
+            if (decision == "double") ;
+            {
+                die1.RollDie();
+                die2.RollDie();
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
